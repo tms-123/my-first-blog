8b22498070b0001345b4954038ed7266d398a94b
Your Name <tom.sharpe@beauhurst.com>
2024-06-11 10:49:07 +0100

    updated path for template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
 def post_list(request):
-    return render(request, 'my-first-blog/blog/post_list.html', {})
+    return render(request, 'my-first-blog/templates/blog/post_list.html', {})
