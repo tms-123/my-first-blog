90cd7df5f151309a8da38f7d93d9e85828c6f825
Your Name <tom.sharpe@beauhurst.com>
2024-07-02 21:15:58 +0100

    user login changes

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+ from django.contrib.auth.models import AbstractUser
+from django.db import models
+class CustomUser(AbstractUser):
+    class Meta:
+        db_table = 'auth_user'
